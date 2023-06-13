f5fded7a836c3cafe4422ef0881a303d0d97667e
Nick Lafauci <nicholas.lafauci@gmail.com>
2023-06-13 12:17:50 -0400

    refactored css

--- script.js
+++ script.js
@@ -1,30 +1,3 @@
-var player = document.getElementById('player')
-var hill = document.getElementById('hill')
-var lava = document.getElementById('lava')
-
-function jump(){
-  if(player.classList !="animate"){
-    player.classList.add("animate");
-  }
-  setTimeout(function(){
-    player.classList.remove("animate");
-    counter++;
-  } 500;)
-}
-
-var lose = setInterval(function(){
-  var playerTop =
-  parseInt(window.getComputedStyle(player).
-    getPropertyValue("top"));
-  var blockLeft =
-  parseInt(window.getComputedStyle(hill).
-    getPropertyValue("left")
-  if(blockLeft<20 && blockLeft>0 && playerTop>=130){
-    obstacle.style.animation = "none";
-    obstacle.style.display = "none";
-  }
-}, 10);
-
-resetButton.addEventListener('click', function () {
-  resetGame();
-});
+const player = document.getElementById('player')
+const hill = document.getElementById('hill')
+const lava = document.getElementById('lava')
