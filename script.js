5ad549763fb48a6d29c546a160d63067b53553e8
Nick Lafauci <nicholas.lafauci@gmail.com>
2023-06-17 11:50:36 -0400

    code refactoring

--- script.js
+++ script.js
@@ -4,41 +4,65 @@ document.addEventListener('DOMContentLoaded', () => {
   const startMenu = document.querySelector('.start-menu');
   const startButton = document.querySelector('.start');
   const gameSpace = document.querySelector('.game-space');
+  const hills = document.querySelectorAll('.hill');
+  const gameOverDiv = document.querySelector('.game-over');
+  const gameOverHeading = document.querySelector('.game-over-heading');
+  const restartButton = document.querySelector('.again');
   let gravity = 2;
-    
-function jump() {
+  let score = 0;
+  let gameStarted = false;
+
+  function jump() {
     player.style.transform = 'translateY(-100px)';
-}
+  }
 
-function fall() {
+  function fall() {
     player.style.transform = 'translateY(0)';
-}
+    checkCollision();
+  }
 
-document.addEventListener('keydown', (event) => {
-    if (event.key === 'ArrowUp' || event.key === ' ') {
-        jump();
-    }
-});
-
-    
-    // Lines 37-46 derived from Endless Runner Reference Video
-    // function lose = setInterval(function(){
-    //   const playerTop =
-    //   parseInt(window.getComputedStyle(player.
-    //     getPropertyValue("top"));
-    //     const blockLeft = 
-    //     parseInt(window.getComputedStyle.))
-    // })
-    
-    setInterval(fall, 1050);
-
-    // resetButton.addEventListener('click', function () {
-    //     resetGame();
-    // });
-
-    startButton.addEventListener('click', () => {
-      startMenu.style.display = 'none';
-      gameSpace.style.position = 'fixed';
+  function checkCollision() {
+    const playerRect = player.getBoundingClientRect();
+    hills.forEach((hill) => {
+      const hillRect = hill.getBoundingClientRect();
+      if (isColliding(playerRect, hillRect)) {
+        gameOver();
+      }
     });
-    
+  }
+
+  function isColliding(rect1, rect2) {
+    return (
+      rect1.left < rect2.right &&
+      rect1.right > rect2.left &&
+      rect1.top < rect2.bottom &&
+      rect1.bottom > rect2.top
+    );
+  }
+
+  function gameOver() {
+    gameSpace.style.display = 'none';
+    gameOverDiv.style.display = 'block';
+  }
+
+  document.addEventListener('keydown', (event) => {
+    if (gameStarted && (event.key === 'ArrowUp' || event.key === ' ')) {
+      jump();
+    }
   });
+
+  setInterval(fall, 1050);
+
+  startButton.addEventListener('click', () => {
+    startMenu.style.display = 'none';
+    gameSpace.style.position = 'fixed';
+    gameStarted = true;
+  });
+
+  restartButton.addEventListener('click', () => {
+    startMenu.style.display = 'block';
+    gameSpace.style.display = 'none';
+    gameOverDiv.style.display = 'none';
+    gameStarted = false;
+  });
+});
