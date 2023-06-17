2ab43928e8103d9eed792c9370d8e884deb36b80
Nick Lafauci <nicholas.lafauci@gmail.com>
2023-06-17 12:32:07 -0400

    game character collision works!

--- script.js
+++ script.js
@@ -1,57 +1,75 @@
 /* partial javascript taken from chatgpt*/
 document.addEventListener('DOMContentLoaded', () => {
-  const player = document.querySelector('.player');
+  const player = document.querySelector('.dino');
   const startMenu = document.querySelector('.start-menu');
   const startButton = document.querySelector('.start');
   const gameSpace = document.querySelector('.game-space');
-  const hills = document.querySelectorAll('.hill');
+  const hills = document.querySelectorAll('.grass.hill img');
   const gameOverDiv = document.querySelector('.game-over');
   const gameOverHeading = document.querySelector('.game-over-heading');
   const restartButton = document.querySelector('.again');
   let gravity = 2;
   let score = 0;
   let gameStarted = false;
+  let jumping = false;
 
   function jump() {
+    if (jumping) {
+      return;
+    }
+    jumping = true;
     player.style.transform = 'translateY(-100px)';
+    setTimeout(() => {
+      player.style.transform = 'translateY(0)';
+      jumping = false;
+      if (gameStarted) {
+        checkCollision();
+      }
+    }, 500);
   }
 
   function fall() {
+    if (jumping) {
+      return;
+    }
     player.style.transform = 'translateY(0)';
-    checkCollision();
+    if (gameStarted) {
+      checkCollision();
+    }
   }
 
   function checkCollision() {
     const playerRect = player.getBoundingClientRect();
+    const playerBottom = playerRect.top + playerRect.height;
+
     hills.forEach((hill) => {
       const hillRect = hill.getBoundingClientRect();
-      if (isColliding(playerRect, hillRect)) {
+      const hillTop = hillRect.top;
+      const hillBottom = hillRect.top + hillRect.height;
+
+      if (
+        playerBottom >= hillTop &&
+        playerRect.left < hillRect.right &&
+        playerRect.right > hillRect.left
+      ) {
         gameOver();
       }
     });
   }
 
-  function isColliding(rect1, rect2) {
-    return (
-      rect1.left < rect2.right &&
-      rect1.right > rect2.left &&
-      rect1.top < rect2.bottom &&
-      rect1.bottom > rect2.top
-    );
-  }
-
   function gameOver() {
+    gameStarted = false;
     gameSpace.style.display = 'none';
     gameOverDiv.style.display = 'block';
   }
 
-  document.addEventListener('keydown', (event) => {
+  window.addEventListener('keydown', (event) => {
     if (gameStarted && (event.key === 'ArrowUp' || event.key === ' ')) {
       jump();
     }
   });
 
-  setInterval(fall, 1050);
+  setInterval(fall, 500);
 
   startButton.addEventListener('click', () => {
     startMenu.style.display = 'none';
